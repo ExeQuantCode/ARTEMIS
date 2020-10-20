a0ea3a9acdd51ab5baff3549616c2cc8d56254eb
nedtaylor <nedttaylor@gmail.com>
2020-10-20 07:19:46 +0100

    Fixed compression for MacOS

--- tools/compress.sh
+++ tools/compress.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 #executing this tars up the code directory.
 
-scriptdir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/"
-codedir=$(sed 's|tools/||' <<<"$scriptdir")
+OS=$(uname)
+case "$OS" in
+	"Darwin")
+	    echo "OS: Darwin, MacOS"
+	    scriptdir="$(pwd "$(dirname "${BASH_SOURCE[0]}")")/"
+	    codedir=$(sed 's|tools/||' <<<"$scriptdir")
+	    tarname=$(sed 's|/$||;s|.*/D||' <<<"$codedir")".tar.gz"
+	    ;;
+	"Linux")
+	    echo "OS: Linux"
+	    scriptdir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/"
+	    codedir=$(sed 's|tools/||' <<<"$scriptdir")
+	    tarname=$(sed 's|/$||;s|.*/D\?||' <<<"$codedir")".tar.gz"
+	    ;;
+	*)
+	    echo "Operating system ($OS) could not be defined"	    
+	    exit 1
+esac
+
+#scriptdir="$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")/"
+#codedir=$(sed 's|tools/||' <<<"$scriptdir")
 codeend=$(sed 's|/.*/\(.*\)/|\1|' <<<"$codedir")
-tarname=$(sed 's|/$||;s|.*/D\?||' <<<"$codedir")".tar.gz"
 tardir=$(sed 's|/$||;s|\(.*/\).*|\1|' <<<"$codedir")
-tarpath=$tardir$tarname
+tarpath="$tardir$tarname"
 
 #cd $codedir
 
 #echo "tar -czvf $tarpath -C $tardir $codeend"
 #tar --exclude-backups -czvf $tarpath -C $tardir $codeend #$scriptdir
-tar --exclude="*#" --exclude="*.#*" --exclude="*~" --exclude="TODO" --exclude="OLD_*" --exclude="*.aux" --exclude="*.log" --exclude="*.out" --exclude="*.toc" --exclude="*.txt" --exclude="${codeend}/bin" --exclude="*.tar.gz" --exclude="${codeend}/src/archive" --exclude="${codeend}/obj" -czvf $tarpath -C $tardir $codeend #$scriptdir
+tar --exclude="*#" --exclude="*.#*" --exclude="*~" --exclude="TODO" --exclude="OLD_*" --exclude="*.aux" --exclude="*.log" --exclude="*.out" --exclude="*.toc" --exclude="*.txt" --exclude="${codeend}/bin" --exclude="*.tar.gz" --exclude="${codeend}/src/archive" --exclude="${codeend}/obj" --exclude="${codeend}/.git" --exclude="*.gitignore" -czvf $tarpath -C $tardir $codeend
 mv $tarpath ../.
 echo "made tar file $tarname"
