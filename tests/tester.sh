14f1e451ab001375951c00422945447d1913966c
Ned Taylor <n.t.taylor@exeter.ac.uk>
2021-11-10 16:08:14 +0000

    Implement tester script

--- tests/tester.sh
+++ tests/tester.sh
@@ -6,15 +6,28 @@ home_dir=$(pwd|sed 's_[/]tests[/]*$__')
 ARTEMIS=$home_dir/bin/artemis
 echo $home_dir
 echo $ARTEMIS
-gfortran --version
-## NED, FOR HOME COMPUTER, COMPILE ARTEMIS USING:
-##make FC=gfortran-mp-10
 
 test_dirs=$(ls -d */)
 echo $test_dirs
 for dir in $test_dirs; do
     cd $test_dir/$dir
     echo $(pwd)
-    $ARTEMIS -f param.in
+    #if [[ "$dir" != "identify_terminations/" ]]; then
+    #	continue
+    #fi
+    rm diff.txt
+    $ARTEMIS -f param.in >> "test.out"
+    POSCARS=$(find D* -name "POSCAR*" -not -path "DCHECK/*" -not -path "POSCAR*")
+    for POSCAR in $POSCARS; do
+	path=$(echo ${POSCAR} | sed 's_^[A-Za-z]*/_DCHECK/_')
+	diff $POSCAR $path  >> "diff.txt"
+    done
+    if [ ! -s "diff.txt" ]; then
+	echo "This test is consistent"
+    else
+	echo "This test has differences with what is expected, please check diff.txt and resolve the issues"
+    fi
+    echo
+    
 done
 
